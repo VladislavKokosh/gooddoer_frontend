9d7dedc68a2314455d3cf13a27a960dff6f32429
Vladislav Kokosh <v.kokosh@pixelplex.io>
2023-06-10 16:51:08 +0300

    Refactoring

---
+++ src/components/header/burger-menu/burger-menu.tsx
@@ -0,0 +1,9 @@
+import MenuIcon from "@mui/icons-material/Menu";
+
+import "./burger-menu.scss";
+
+const BurgerMenu = () => {
+	return <MenuIcon className="menu-burger" />;
+};
+
+export { BurgerMenu };
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,13 +1,20 @@
+import { useSize } from "ahooks";
+import { useRef } from "react";
+
 import { ReactComponent as Logo } from "../../assets/icons/logo.svg";
 
+import { BurgerMenu } from "./burger-menu";
 import "./header.scss";
 import { Menu } from "./menu";
 
 const Header = () => {
+	const ref = useRef(null);
+	const size = useSize(ref);
+
 	return (
-		<div className="header">
+		<div className="header" ref={ref}>
 			<Logo className="header_logo" />
-			<Menu />
+			{size?.width && size.width > 750 ? <Menu /> : <BurgerMenu />}
 		</div>
 	);
 };
