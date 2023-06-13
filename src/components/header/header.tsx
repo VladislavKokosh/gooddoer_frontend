64b5821138a861211c31f31e1a80c6fa3c6e2c3b
Vladislav Kokosh <v.kokosh@pixelplex.io>
2023-06-13 19:43:52 +0300

    Add button to connect wallet

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,10 @@
 import { useSize } from "ahooks";
 import { useRef } from "react";
+import { useSelector } from "react-redux";
 
 import { ReactComponent as Logo } from "../../assets/icons/logo.svg";
+import { getProviderState, getSelectedAddress } from "../../store";
+import { Button } from "../button";
 
 import { BurgerMenu } from "./burger-menu";
 import "./header.scss";
@@ -10,11 +13,24 @@ import { Menu } from "./menu";
 const Header = () => {
 	const ref = useRef(null);
 	const size = useSize(ref);
+	const providerSelector = useSelector(getProviderState);
+	const addressSelector = useSelector(getSelectedAddress);
+
+	const handleClick = () => {
+		providerSelector?.provider?.request({
+			method: "eth_requestAccounts",
+			params: [],
+		});
+	};
 
 	return (
 		<div className="header" ref={ref}>
 			<Logo className="header_logo" />
 			{size?.width && size.width > 750 ? <Menu /> : <BurgerMenu />}
+			<Button
+				title={addressSelector ? addressSelector : "Connect wallet"}
+				onClick={addressSelector ? undefined : handleClick}
+			/>
 		</div>
 	);
 };
--- src/store/provider/provider.selectors.ts
+++ src/store/provider/provider.selectors.ts
@@ -1,5 +1,6 @@
 import { TStore } from "../store";
 
 const getProviderState = ({ provider }: TStore) => provider;
+const getSelectedAddress = ({ provider }: TStore) => provider.selectedAddress;
 
-export { getProviderState };
+export { getProviderState, getSelectedAddress };
