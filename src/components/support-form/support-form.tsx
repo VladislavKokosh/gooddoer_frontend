23f85c628d1734d85969346ba0cff19174d81d0b
Vladislav Kokosh <v.kokosh@pixelplex.io>
2023-09-06 17:28:28 +0300

    Fix

--- src/components/support-form/support-form.tsx
+++ src/components/support-form/support-form.tsx
@@ -5,7 +5,9 @@ import "./support-form.scss";
 const SupportForm = () => {
 	return (
 		<div className="support">
-			<div className="support-title">How can we help you?</div>
+			<div className="support-title">
+				<span>How can we help you?</span>
+			</div>
 			<div className="support-items">
 				{items.map((item) => (
 					<Item key={item.id} {...item} />
